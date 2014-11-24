fcdc3a03f00966c2629e32786c7c1b896e526674
canvasowl <jefrycruz88@gmail.com>
2014-11-24 12:32:18 -0500

    Added the Hubspot helper classes

--- app/models/Hubspot.php
+++ app/models/Hubspot.php
@@ -4,13 +4,18 @@
  * Hubspot related data that needs to be retrived
  * or posted. 
  */
-class Hubspot{
+class Hubspot {
 	
-	protected $api_key 		= "";
-	protected $oAuth_key	= "";
+	protected $api_key 		= "demo";
+	protected $oAuth_key	= "demooooo-oooo-oooo-oooo-oooooooooooo";
 
 
-	//public static getLeads(){}
+	public static function getLeads(){
+		$contacts = new HubSpot_Contacts("demo");
+		$contacts_batch1 = $contacts->get_all_contacts(array('count' => '100'));
+		
+		return json_encode($contacts_batch1->contacts);		
+	}
 
 	//public static getTraffic(){}
 
--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('/', function() { return View::make('index'); });
+
+Route::get('/leads', function() {
+	return Hubspot::getLeads();
+});
