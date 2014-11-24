77f31b3132b823e51e6b058d406158077aabc6f5
canvasowl <jefrycruz88@gmail.com>
2014-11-24 11:55:24 -0500

    Added Bootstrap, and created a simple master template file

---
+++ app/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.master')
+
+@section('content')
+	<div class="row">
+		<div class="col-xs-12">
+			<p>This is appended to the master sidebar.</p>		
+		</div>
+	</div>    
+@stop
---
+++ app/views/templates/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Enterpise</title>
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+	@section('content')
+        
+    @show	
+</div>
+
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
