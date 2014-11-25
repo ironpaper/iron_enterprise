b4b374d3db5fd05fc91234f3533a92c5952b36be
canvasowl <jefrycruz88@gmail.com>
2014-11-25 11:34:20 -0500

    Added panel decrsiptions

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,7 +10,7 @@
 
 	<div class="row">
 		
-		<div class="col-xs-3">
+		<div class="col-xs-12">
 			<div class="panel panel-default">
 			  <div class="panel-heading"><center>Traffic</center></div>
 			  <div class="panel-body">
@@ -18,27 +18,30 @@
 			  </div>
 			</div>					
 		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-4">
 			<div class="panel panel-default">
 			  <div class="panel-heading"><center>Leads</center></div>
 			  <div class="panel-body">
-			    <center><h2>{{ $leads }}</h2></center>
-			  </div>
+			    <center><h2>{{ $leads }}</h2>
+			    <p class="dimmned">A lead is a contact that expresses interest in your brand or off er andprovides enough basic informationto begin a validation process</p>
+			  </div></center>
 			</div>					
 		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-4">
 			<div class="panel panel-default">
 			  <div class="panel-heading"><center>MQL</center></div>
 			  <div class="panel-body">
-			    <center><h2>{{ $mql }}</h2></center>
-			  </div>
+			    <center><h2>{{ $mql }}</h2>
+			    <p class="dimmned">An MQL is a transition staterepresenting either increasedvalidation or a deeper expressionof interest.An MQL is a lead thatprovides some crucial validationallowing for a future salesopportunity.</p>
+			  </div></center>
 			</div>					
 		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-4">
 			<div class="panel panel-default">
 			  <div class="panel-heading"><center>SQL</center></div>
 			  <div class="panel-body">
-			    <center><h2>{{ $sql }}</h2></center>
+			    <center><h2>{{ $sql }}</h2>
+			    <p class="dimmned">An SQL is a validated lead that has also expressed interest in a proposal or deal exploration.</p></center>
 			  </div>
 			</div>					
 		</div>	
